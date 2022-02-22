ba6fa6d66c7272d28cb7f380137dd2a52b71b813
Krishna sai Patel <herorocks56@gmail.com>
2022-02-22 13:37:16 -0800

    backend basic configuration done

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const mysql = require('mysql')
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(3001, () => {
+    console.log("Server listening on port 3001");
+})
