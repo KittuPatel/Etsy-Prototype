042eb70f29434e10acf6e8c06fefc1f3bd688a31
Krishna sai Patel <herorocks56@gmail.com>
2022-02-23 21:51:11 -0800

    footer styles fixed

--- frontend/etsy/src/components/Footer.js
+++ frontend/etsy/src/components/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
          
-    <footer className="footer">
+    <footer className="footer" style={{backgroundColor: "#1e1e27"}}>
     <div className="container footerContainer">
         <div className="row">
             <div className="col-lg-12">
             <div className="footer_nav_container">
-                <div className="cr">©2022 All Rights Reserverd. Made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="/">Krishna Sai</a></div>
+                <div className="cr"></div>
             </div>
             </div>
               </div>
@@ -16,33 +16,31 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="top_nav_left">free shipping on all u.s orders over $50</div>
-                    </div>
-                    <div className="col-md-6 text-right">
                         <div className="top_nav_right">
                             <ul className="top_nav_menu">
-                                <li className="currency">
-                                    <a href="/">
-                                        usd
-                                        <i className="fa fa-angle-down"></i>
-                                    </a>
-                                    {/* <ul className="currency_selection">
-                                        <li><a href="/">cad</a></li>
-                                        <li><a href="/">aud</a></li>
-                                        <li><a href="/">eur</a></li>
-                                        <li><a href="/">gbp</a></li>
-                                    </ul> */}
-                                </li>
+                                
                                 <li className="language">
                                     <a href="/">
-                                        English
-                                        <i className="fa fa-angle-down"></i>
+                                        United States
+                                        {/* <i className="fa fa-angle-down"></i> */}
                                     </a>
                                     {/* <ul className="language_selection">
                                         <li><a href="/">French</a></li>
                                         <li><a href="/">Italian</a></li>
                                         <li><a href="/">German</a></li>
                                         <li><a href="/">Spanish</a></li>
+                                    </ul> */}
+                                </li>
+                                <li className="currency">
+                                    <a href="/">
+                                        usd
+                                        {/* <i className="fa fa-angle-down"></i> */}
+                                    </a>
+                                    {/* <ul className="currency_selection">
+                                        <li><a href="/">cad</a></li>
+                                        <li><a href="/">aud</a></li>
+                                        <li><a href="/">eur</a></li>
+                                        <li><a href="/">gbp</a></li>
                                     </ul> */}
                                 </li>
                                 <li className="account">
@@ -57,6 +55,9 @@ const Footer = () => {
                                 </li>
                             </ul>
                         </div>
+                    </div>
+                    <div className="col-md-6 text-right">
+                        <div className="top_nav_left">©2022 All Rights Reserverd. Made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://krishnasai.me/">Krishna Sai</a></div>
                     </div>
                 </div>
             </div>
