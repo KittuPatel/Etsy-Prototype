829010e849d2690533ddbf1dd5ca504955fe1d96
Krishna sai Patel <herorocks56@gmail.com>
2022-02-23 21:00:50 -0800

    added Header.js & Footer.js

--- frontend/etsy/src/App.js
+++ frontend/etsy/src/App.js
@@ -1,97 +1,11 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="MainDiv">
-     
-        <header class="header trans_300">
-         
-          <div class="top_nav">
-            <div class="container">
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="top_nav_left">free shipping on all u.s orders over $50</div>
-                </div>
-                <div class="col-md-6 text-right">
-                  <div class="top_nav_right">
-                    <ul class="top_nav_menu">
-                    
-                      <li class="currency">
-                        <a href="/">
-                          usd
-                          <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="currency_selection">
-                          <li><a href="/">cad</a></li>
-                          <li><a href="/">aud</a></li>
-                          <li><a href="/">eur</a></li>
-                          <li><a href="/">gbp</a></li>
-                        </ul>
-                      </li>
-                      <li class="language">
-                        <a href="/">
-                          English
-                          <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="language_selection">
-                          <li><a href="/">French</a></li>
-                          <li><a href="/">Italian</a></li>
-                          <li><a href="/">German</a></li>
-                          <li><a href="/">Spanish</a></li>
-                        </ul>
-                      </li>
-                      <li class="account">
-                        <a href="/">
-                          My Account
-                          <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="account_selection">
-                          <li><a href="/"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-                          <li><a href="/"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        
-          <div class="main_nav_container">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-12 text-right">
-                  <div class="logo_container">
-                    <a href="/">Jassa<span>Rich</span></a>
-                  </div>
-                  <nav class="navbar">
-                    <ul class="navbar_menu">
-                      <li><a href="/">home</a></li>
-                      <li><a href="/">shop</a></li>
-                      <li><a href="/">promotion</a></li>
-                      <li><a href="/">pages</a></li>
-                      <li><a href="/">blog</a></li>
-                      <li><a href="/">contact</a></li>
-                    </ul>
-                    <ul class="navbar_user">
-                      <li><a href="/"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-                      <li><a href="/"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-                      <li class="checkout">
-                        <a href="/">
-                          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                          <span id="checkout_items" class="checkout_items">2</span>
-                        </a>
-                      </li>
-                    </ul>
-                    <div class="hamburger_container">
-                      <i class="fa fa-bars" aria-hidden="true"></i>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
+      <Header/>
         <div class="fs_menu_overlay"></div>
         <div class="hamburger_menu">
           <div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
@@ -668,40 +582,7 @@ function App() {
             </div>
           </div>
         </div>
-      
-        <footer class="footer">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-                  <ul class="footer_nav">
-                    <li><a href="/">Blog</a></li>
-                    <li><a href="/">FAQs</a></li>
-                    <li><a href="/">Contact us</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-                  <ul>
-                    <li><a href="/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li><a href="/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li><a href="/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                    <li><a href="/"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-                    <li><a href="/"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="footer_nav_container">
-                  <div class="cr">©2021 All Rights Reserverd. Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="/">Jassa</a> &amp; Loved by <a href="https://therichpost.com">Jassa</a></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+      <Footer />
       </div>
   );
 }
