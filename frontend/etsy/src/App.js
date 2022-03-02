70f64e8cd5250c52c889d5f99d70a380ae339bc2
Krishna sai Patel <herorocks56@gmail.com>
2022-03-01 22:52:46 -0800

    added react router

--- frontend/etsy/src/App.js
+++ frontend/etsy/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
