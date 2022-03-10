d65b81922ca320ed9a83f98fa719dcccb70d5b5f
Krishna sai Patel <herorocks56@gmail.com>
2022-03-10 01:12:28 -0800

    Header fixed, added links to register and login

--- frontend/etsy/src/components/Header.js
+++ frontend/etsy/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -17,7 +18,8 @@ const Header = () => {
                                 <li><a href="/">promotion</a></li>
                                 <li><a href="/">pages</a></li>
                                 <li><a href="/">blog</a></li>
-                                <li><a href="/">contact</a></li>
+                                <li><Link to="/register"><i className="fa fa-user-plus" aria-hidden="true"></i>  Register</Link></li>
+                                <li><Link to="/login"><i className="fa fa-sign-in" aria-hidden="true"></i>  Sign In</Link></li>
                             </ul>
                             <ul class="navbar_user">
                                 <li><a href="/"><i class="fa fa-search" aria-hidden="true"></i></a></li>
@@ -36,7 +38,55 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-        </div>
+          </div>
+              <div className="fs_menu_overlay"></div>
+    <div className="hamburger_menu">
+      <div className="hamburger_close"><i className="fa fa-times" aria-hidden="true"></i></div>
+      <div className="hamburger_menu_content text-right">
+        <ul className="menu_top_nav">
+          <li className="menu_item has-children">
+            <a href="/">
+              usd
+              <i className="fa fa-angle-down"></i>
+            </a>
+            <ul className="menu_selection">
+              <li><a href="/">cad</a></li>
+              <li><a href="/">aud</a></li>
+              <li><a href="/">eur</a></li>
+              <li><a href="/">gbp</a></li>
+            </ul>
+          </li>
+          <li className="menu_item has-children">
+            <a href="/">
+              English
+              <i className="fa fa-angle-down"></i>
+            </a>
+            <ul className="menu_selection">
+              <li><a href="/">French</a></li>
+              <li><a href="/">Italian</a></li>
+              <li><a href="/">German</a></li>
+              <li><a href="/">Spanish</a></li>
+            </ul>
+          </li>
+          {/* <li className="menu_item has-children">
+            {/* <a href="#">
+              My Account
+              <i className="fa fa-angle-down"></i>
+            </a> */}
+            {/* <ul className="menu_selection">  */}
+
+            {/* </ul> */}
+          {/* </li> */}
+          <li className="menu_item"><a href="/">home</a></li>
+          <li className="menu_item"><a href="/">shop</a></li>
+          <li className="menu_item"><a href="/">promotion</a></li>
+          <li className="menu_item"><a href="/">pages</a></li>
+          <li className="menu_item"><a href="/">blog</a></li>
+              <li className="menu_item"></li>
+              <li className="menu_item"><Link to="/login"><i className="fa fa-sign-in" aria-hidden="true"></i>Sign In</Link></li>
+        </ul>
+      </div>
+    </div>
   </header>
   )
 }
--- frontend/etsy/src/components/Home.js
+++ frontend/etsy/src/components/Home.js
@@ -1,59 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <>
-            <Header />
-    <div className="fs_menu_overlay"></div>
-    <div className="hamburger_menu">
-      <div className="hamburger_close"><i className="fa fa-times" aria-hidden="true"></i></div>
-      <div className="hamburger_menu_content text-right">
-        <ul className="menu_top_nav">
-          <li className="menu_item has-children">
-            <a href="/">
-              usd
-              <i className="fa fa-angle-down"></i>
-            </a>
-            <ul className="menu_selection">
-              <li><a href="/">cad</a></li>
-              <li><a href="/">aud</a></li>
-              <li><a href="/">eur</a></li>
-              <li><a href="/">gbp</a></li>
-            </ul>
-          </li>
-          <li className="menu_item has-children">
-            <a href="/">
-              English
-              <i className="fa fa-angle-down"></i>
-            </a>
-            <ul className="menu_selection">
-              <li><a href="/">French</a></li>
-              <li><a href="/">Italian</a></li>
-              <li><a href="/">German</a></li>
-              <li><a href="/">Spanish</a></li>
-            </ul>
-          </li>
-          <li className="menu_item has-children">
-            <a href="/">
-              My Account
-              <i className="fa fa-angle-down"></i>
-            </a>
-            <ul className="menu_selection">
-              <li><a href="/"><i className="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-              <li><a href="/"><i className="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
-            </ul>
-          </li>
-          <li className="menu_item"><a href="/">home</a></li>
-          <li className="menu_item"><a href="/">shop</a></li>
-          <li className="menu_item"><a href="/">promotion</a></li>
-          <li className="menu_item"><a href="/">pages</a></li>
-          <li className="menu_item"><a href="/">blog</a></li>
-          <li className="menu_item"><a href="/">contact</a></li>
-        </ul>
-      </div>
-    </div>
+      <Header />
   
     <div className="main_slider" style={{backgroundImage:"url(assets/images/slider_1.jpg)"}}>
       <div className="container fill_height">
