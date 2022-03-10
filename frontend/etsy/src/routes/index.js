55b910c38dba43b43b71ce1c51d553014a74fa23
Krishna sai Patel <herorocks56@gmail.com>
2022-03-10 00:01:49 -0800

    routes config done

--- frontend/etsy/src/routes/index.js
+++ frontend/etsy/src/routes/index.js
@@ -1,18 +1,29 @@
+import Register from '../components/Register';
+import Login from '../components/Login';
+import App from '../components/App';
+import ShopName from '../components/ShopName';
+
 const routes = [
     {
         path: '/auth/register',
         component: Register,
+        title: 'Register',
     },
     {
         path: '/auth/login',
         component: Login,
+        title: 'Login',
     },
     {
         path: '/',
         component: App,
+        title: 'Etsy',
     },
     {
         path: '/create-shop',
         component: ShopName,
+        title: 'Create Shop',
     },
 ]
+
+export default routes
