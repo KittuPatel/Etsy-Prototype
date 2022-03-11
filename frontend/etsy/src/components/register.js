83bb8bc755e17b3e90f117f11eee28ca931af9c3
Krishna sai Patel <herorocks56@gmail.com>
2022-03-10 19:51:36 -0800

    register page validations done

--- frontend/etsy/src/components/register.js
+++ frontend/etsy/src/components/register.js
@@ -1,51 +1,104 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Register = () => {
-  return (
-    <div className="d-lg-flex half">
-    <div className="bg order-2 order-md-1" style= {{
-  backgroundImage: "url(" + "https://images.pexels.com/photos/7585763/pexels-photo-7585763.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" + ")",}}></div>
-    <div className="contents order-1 order-md-2">
+  const [form, setForm] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  })
 
-      <div className="container">
-        <div className="row align-items-center justify-content-center">
-          <div className="col-md-7">
-            <h3 className='mb-4'>Signup to <span className='etsy-logo'>Etsy</span></h3>
-            {/* <p className="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p> */}
-            <form action="#" method="post">
-              <div className="form-group username">
-                <label for="username">Fullname</label>
-                <input type="text" className="form-control" style={{fontWeight: 'normal'}} placeholder="Enter your Name" id="name" />
-              </div>
-              <div className="form-group email">
-                <label for="username">Email</label>
-                <input type="email" className="form-control" placeholder="your-email@gmail.com" id="email" />
-              </div>
-              <div className="form-group password mb-3">
-                <label for="password">Password</label>
-                <input type="password" className="form-control" placeholder="Enter Password" id="password" />
-              </div>
-              
-              <div className="d-flex mb-2 align-items-center">
+  const handleChange = (e) => {
+    const value = e.target.value
+    setForm({
+      ...form,
+      [e.target.name]: value,
+    })
+    console.log(form)
+  }
+
+  return (
+    <div className='d-lg-flex half'>
+      <div
+        className='bg order-2 order-md-1'
+        style={{
+          backgroundImage:
+            "url(" +
+            "https://images.pexels.com/photos/7585763/pexels-photo-7585763.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" +
+            ")",
+        }}
+      ></div>
+      <div className='contents order-1 order-md-2'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-center'>
+            <div className='col-md-7'>
+              <h3 className='mb-4'>
+                Signup to <span className='etsy-logo'>Etsy</span>
+              </h3>
+              {/* <p className="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p> */}
+              <form action='#' method='post'>
+                <div className='form-group username'>
+                  <label for='username'>Fullname</label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    style={{ fontWeight: "normal" }}
+                    placeholder='Enter your Name'
+                    id='name'
+                    name='fullName'
+                    value={form.fullName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='form-group email'>
+                  <label for='username'>Email</label>
+                  <input
+                    type='email'
+                    className='form-control'
+                    placeholder='your-email@gmail.com'
+                    id='email'
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='form-group password mb-3'>
+                  <label for='password'>Password</label>
+                  <input
+                    type='password'
+                    className='form-control'
+                    placeholder='Enter Password'
+                    id='password'
+                    name='password'
+                    value={form.password}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className='d-flex mb-2 align-items-center'>
                   <p>You agree to the Terms & Conditions by signing up.</p>
-                {/* <label className="control control--checkbox mb-0"><span className="caption">Remember me</span>
+                  {/* <label className="control control--checkbox mb-0"><span className="caption">Remember me</span>
                   <input type="checkbox" checked="checked"/>
                   <div className="control__indicator"></div>
                 </label> */}
-                {/* <span className="ml-auto"><a href="#" className="forgot-pass">Forgot Password</a></span>  */}
-              </div>
+                  {/* <span className="ml-auto"><a href="#" className="forgot-pass">Forgot Password</a></span>  */}
+                </div>
 
-              <input type="submit" value="Sign up" className="btn btn-block btn-primary" />
+                <input
+                  type='submit'
+                  value='Sign up'
+                  disabled={!form.fullName || !form.email || !form.password}
+                  className='btn btn-block btn-primary'
+                />
                 <br />
-              <p className="text-center">Already have an account? <a href="/login">Login</a></p>
-            </form>
+                <p className='text-center'>
+                  Already have an account? <a href='/login'>Login</a>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    
-  </div>
   )
 }
 
