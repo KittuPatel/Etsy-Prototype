5726686f8dbd10c4a496bd7917fd86c1795a20a2
Krishna sai Patel <herorocks56@gmail.com>
2022-03-20 19:36:10 -0700

    everything works foog

--- backend/createDefaultCategories.js
+++ backend/createDefaultCategories.js
@@ -1,28 +1,31 @@
-import mysql from 'mysql'
+import mysql from "mysql"
 
 var connection = mysql.createPool({
-  "host": "etsydb.ciubgzni56ko.us-east-1.rds.amazonaws.com",
-  "user": "admin",
-  "password": "admin123",
-  "port": "3306",
-  "database": "etsydb"
-});
+  host: "etsy-database.caphir2hzg3k.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "admin123",
+  port: "3306",
+  database: "etsydb",
+})
 
 var categories = [
-    ["ct-1234", "mobiles", null, null, true],
-    ["ct-1235", "tops", null, null, true],
-    ["ct-12346", "Home Decore", null, null, true]
-] 
+  ["ct-1234", "mobiles", null, null, true],
+  ["ct-1235", "tops", null, null, true],
+  ["ct-12346", "Home Decore", null, null, true],
+]
 connection.getConnection((err) => {
   if (err) {
     console.log("######### error occurred")
-    throw 'Error occoured' + err;
+    throw "Error occoured" + err
   }
-  console.log("Connection Created");
+  console.log("Connection Created")
   categories.forEach((category) => {
-    connection.query('INSERT INTO category VALUES (?,?,?,?,?)', category, (err, results, fields)=>{
-      console.log(err, results)
-    })
+    connection.query(
+      "INSERT INTO category VALUES (?,?,?,?,?)",
+      category,
+      (err, results, fields) => {
+        console.log(err, results)
+      }
+    )
   })
-
-});
+})
